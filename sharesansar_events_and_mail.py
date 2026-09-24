113ad44b82e9fb995fdf1c78ccc341eef3d27cdd
Akhilesh Bikram Sthapit <37375453+bikakhilesh@users.noreply.github.com>
2026-09-24 09:49:51 +0545

    Update sharesansar_events_and_mail.py
    make number of days 4

--- sharesansar_events_and_mail.py
+++ sharesansar_events_and_mail.py
@@ -28,7 +28,7 @@ from openpyxl.styles import Font, PatternFill, Alignment, Border, Side
 NPT = timezone(timedelta(hours=5, minutes=45))
 TODAY_NPT = datetime.now(NPT).replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
 START_DATE = TODAY_NPT - timedelta(days=3)
-NUM_DAYS = 3
+NUM_DAYS = 4
 SAVE_DIR = tempfile.gettempdir()      # runner temp; file only lives in the email
 ALSO_SAVE_CSV = False
 
